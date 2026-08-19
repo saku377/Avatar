f79253840aad45b30719fd9cce9398cc72c933ad
saku377 <roxanahellen2001@gmail.com>
2026-08-19 16:29:20 -0400

    ejecricicos

--- 006D/Avatar/js/functions.js
+++
@@ -1,8 +0,0 @@
-   fetch('https://cl.dolarapi.com/v1/cotizaciones/usd')
-  .then(respuesta => respuesta.json())
-  .then(datos => document.getElementById("saludo").innerHTML=datos.ultimoCierre)
-  .catch(error => console.error('Hubo un error:', error));
-    console.log()
-    // document.getElementById("saludo").innerHTML="Se juega mañana por la noche?";
-
-    
